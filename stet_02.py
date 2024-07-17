c170f5aefac0f7da42321d011df4023322a2f7cb
Enkh <enhmonhbat03@gmail.com>
2024-07-17 14:45:50 +0800

    1

--- stet_02.py
+++ stet_02.py
@@ -25,24 +25,27 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    bodies = body_cascade.detectMultiScale(gray, 1.1, 5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    bodies = body_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    if len(faces) + len(bodies) > 0:
+    num_faces = len(faces)
+    num_bodies = len(bodies)
+
+    if num_faces > 1:
         if detection:
             timer_started = False
         else:
             detection = True
             current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
-            print(f"{current_time}")
+            print(f"Start {current_time}")
     elif detection:
         if timer_started:
             if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
                 detection = False
                 timer_started = False
                 out.release()
-                print(f"{current_time}")
+                current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+                print(f"End {current_time}")
         else:
             timer_started = True
             detection_stopped_time = time.time()
@@ -51,8 +54,7 @@ while True:
         out.write(frame)
 
     for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
-
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
     for (x, y, width, height) in bodies:
         cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
 
